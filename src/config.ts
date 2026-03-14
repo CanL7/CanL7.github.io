926faa797e7f22f5e152d54b6d703ab134ae73d2
CanL7 <lxh3556167382@163.com>
2026-03-14 19:35:00 +0800

    更新一下网页配置~

--- src/config.ts
+++ src/config.ts
@@ -119,10 +119,10 @@ export const siteConfig: SiteConfig = {
 		// 支持单张图片或图片数组，当数组长度 > 1 时自动启用轮播
 		src: {
 			desktop: [
-                "/public/images/sagiri/5.jpg",
-				"/public/images/sagiri/9.jpg",
-				"/public/images/sagiri/11.jpg",
-				"/public/images/sagiri/15.jpg",
+                "/images/sagiri/5.jpg",
+				"/images/sagiri/9.jpg",
+				"/images/sagiri/11.jpg",
+				"/images/sagiri/15.jpg",
 			], // 桌面横幅图片
 			mobile: [
 				"/public/images/sagiri/5.jpg",
@@ -227,16 +227,16 @@ export const siteConfig: SiteConfig = {
 export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 	src: {
 		desktop: [
-			"/public/images/sagiri/5.jpg",
-            "/public/images/sagiri/9.jpg",
-            "/public/images/sagiri/11.jpg",
-            "/public/images/sagiri/15.jpg",
+			"/images/sagiri/5.jpg",
+            "/images/sagiri/9.jpg",
+            "/images/sagiri/11.jpg",
+            "/images/sagiri/15.jpg",
 		], // 桌面横幅图片
 		mobile: [
-			"/public/images/sagiri/5.jpg",
-            "/public/images/sagiri/9.jpg",
-            "/public/images/sagiri/11.jpg",
-            "/public/images/sagiri/15.jpg",
+			"/images/sagiri/5.jpg",
+            "/images/sagiri/9.jpg",
+            "/images/sagiri/11.jpg",
+            "/images/sagiri/15.jpg",
 		], // 移动横幅图片
 	}, // 使用本地横幅图片
 	position: "center", // 壁纸位置，等同于 object-position
