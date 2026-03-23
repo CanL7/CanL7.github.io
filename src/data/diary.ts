6016330f9a425faee89f0e3d851afe4d095ee990
CanL7 <lxh3556167382@163.com>
2026-03-24 00:06:03 +0800

    提交新diary

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -12,20 +12,20 @@ export interface DiaryItem {
 }
 
 // 示例日记数据
-const diaryData: DiaryItem[] = [
-	{
-		id: 1,
-		content:
-			"终于搭好了一个好看的博客！ 加油学习吧",
-		date: "2026-03-12",
-	},
-{
-		id: 2,
-		content:
-			"学着其实并不累 希望自己不要放弃呀！多往图书馆去 寝室学不了！ ",
-		date: "2026-03-17",
-	},
-];
+// const diaryData: DiaryItem[] = [
+// 	{
+// 		id: 1,
+// 		content:
+// 			"终于搭好了一个好看的博客！ 加油学习吧",
+// 		date: "2026-03-12",
+// 	},
+// {
+// 		id: 2,
+// 		content:
+// 			"学着其实并不累 希望自己不要放弃呀！多往图书馆去 寝室学不了！ ",
+// 		date: "2026-03-17",
+// 	},
+// ];
 
 // 获取日记统计数据
 export const getDiaryStats = () => {
